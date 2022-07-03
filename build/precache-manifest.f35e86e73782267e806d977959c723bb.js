ecd972ddb7f1a343bcd918f130478e2b04b14d18
Kunal Vijay <kunalvijay.kv2002@gmail.com>
2022-07-03 23:19:19 +0530

    None

---
+++ build/precache-manifest.f35e86e73782267e806d977959c723bb.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d8958c66f852bbe173ee9445b3deb789",
+    "url": "/Keeper-App/index.html"
+  },
+  {
+    "revision": "7d2c7a43f22c6ea488de",
+    "url": "/Keeper-App/static/js/2.fc26be5f.chunk.js"
+  },
+  {
+    "revision": "7603dcfa7d915fdb1b7e",
+    "url": "/Keeper-App/static/js/main.ada5149c.chunk.js"
+  },
+  {
+    "revision": "660c3a2d9cc725da8ecd",
+    "url": "/Keeper-App/static/js/runtime-main.78711218.js"
+  }
+]);
