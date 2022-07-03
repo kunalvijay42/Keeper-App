ce26e0f2d6bc90a8ba14bcc261c45ee656d3cb0d
Kunal Vijay <kunalvijay.kv2002@gmail.com>
2022-07-03 23:25:13 +0530

    Updates

---
+++ precache-manifest.77a6ca15796fcc8c2bede85e47257362.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "403fc9e1ed58f795c0cd40a6ddaf0b54",
+    "url": "/Keeper-App/index.html"
+  },
+  {
+    "revision": "7d2c7a43f22c6ea488de",
+    "url": "/Keeper-App/static/js/2.fc26be5f.chunk.js"
+  },
+  {
+    "revision": "7603dcfa7d915fdb1b7e",
+    "url": "/Keeper-App/static/js/main.ada5149c.chunk.js"
+  },
+  {
+    "revision": "660c3a2d9cc725da8ecd",
+    "url": "/Keeper-App/static/js/runtime-main.78711218.js"
+  }
+]);
